f2c428dc01e3e3e083c4541939f5cbb398abe025
Liorion Nguyen <liorion.nguyen@gmail.com>
2024-09-27 16:22:37 +0700

    fix: auth

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*',  // Cho phép tất cả các domain
+    origin: '*', 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept,Authorization',
   });  
